f26ba5494b6801cc94d4f150713cc3dbb0560c0f
Ben Zedaker <bzedaker001@defiance.edu>
2021-02-22 11:24:09 -0500

    Added text after game over

--- game.js
+++ game.js
@@ -56,7 +56,7 @@ function showConclusion(score) {
     ctx.font="30px Courier New";
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
-    ctx.fillText("GAME OVER", canvas.width/2, canvas.height/2);
+    ctx.fillText("GAME OVER Better Luck Next Time", canvas.width/2, canvas.height/2);
     ctx.fillText("score: " + score, canvas.width/2, canvas.height/2-40);
     ctx.font="20px Courier New";
     ctx.fillText("press space to start", canvas.width/2, canvas.height/2+80);
